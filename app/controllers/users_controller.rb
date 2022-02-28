cbd35d9a8a69983d91a7bcc4a410c32d9f2abd5d
jesspesale <jess_pesale@aol.com>
2022-02-27 23:05:18 -0500

    added routes for create for user and session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(user_params)
+        
         if user.save
             render json: {status: 201, user: user, logged_in: true}
         else
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :sessions, only: [:create]
+  resources :users, only: [:create]
 
 
 end
