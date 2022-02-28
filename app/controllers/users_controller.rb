b61883553691556365bca8e5702b220bc65cd730
jesspesale <jess_pesale@aol.com>
2022-02-27 23:04:02 -0500

    created user create action in controller with user_params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: {status: 201, user: user, logged_in: true}
+        else
+            render json: {statis: 500, message: "There was an error creating an account"}
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+end
